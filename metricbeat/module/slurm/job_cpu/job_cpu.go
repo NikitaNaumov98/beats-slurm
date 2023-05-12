9941f3d4621fd2b24c199bdc340a7ea7a82cdb25
Nikita Naumov <niknau2009@gmail.com>
2023-05-12 04:57:16 +0300

    small fixes

--- metricbeat/module/slurm/job_cpu/job_cpu.go
+++ metricbeat/module/slurm/job_cpu/job_cpu.go
@@ -35,6 +35,7 @@ type MetricSet struct {
 // New creates a new instance of the MetricSet. New is responsible for unpacking
 // any MetricSet specific configuration options if there are any.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	slurmdir = "/sys/fs/cgroup/memory/slurm"
 
 	config := struct{}{}
 	if err := base.Module().UnpackConfig(&config); err != nil {
